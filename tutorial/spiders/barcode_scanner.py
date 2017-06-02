92597e2ab7d65dca9c2799b511ae918e2a4575f1
Son Nguyen <ntson.sh@gmail.com>
2017-06-01 23:02:03 -0700

    Finish crawling

--- tutorial/spiders/barcode_scanner.py
+++ tutorial/spiders/barcode_scanner.py
@@ -8,10 +8,10 @@ class QuotesSpider(scrapy.Spider):
             }
     def start_requests(self):
         urls = [
-                'https://lb1.icheck.com.vn/scan/8934563151157'
+                'https://mining.icheck.com.vn/popular/category?parent_category_id=13&limit=500'
                 ] 
         for url in urls:
-            yield scrapy.Request(url = url, callback = self.parse, headers = {
+            yield scrapy.Request(url = url, callback = self.parse_category, headers = {
                 'icheck-id':'i-1494218194088',
                 'Authorization':'Basic aWNoZWNrOmlZQUYmO2NCZSNHM2F+RDojaGVjaw=='
                 })
@@ -22,22 +22,31 @@ class QuotesSpider(scrapy.Spider):
         if data and len(data)>0:
             for item in data:
                 code = item['gtin_code']
-                item_url = 'https://lb1.icheck.com.vn/scan/{0}'.format(code)
+                item_url = 'https://lb1.icheck.com.vn/scan/{0}?scan=0'.format(code)
                 yield scrapy.Request(url = item_url, callback = self.parse, headers = {
                     'icheck-id':'i-1494218194088',
                     'Authorization':'Basic aWNoZWNrOmlZQUYmO2NCZSNHM2F+RDojaGVjaw=='
                 })
-
+    def parse_category(self, response):
+        json_response = json.loads(response.body_as_unicode())
+        code_list = json_response['data']
+        for item in code_list:
+            item_url = 'https://lb1.icheck.com.vn/scan/{0}?scan=0'.format(item['gtin_code'])
+            yield scrapy.Request(url = item_url, callback = self.parse, headers = {
+                'icheck-id':'i-1494218194088',
+                'Authorization':'Basic aWNoZWNrOmlZQUYmO2NCZSNHM2F+RDojaGVjaw=='
+            })
+           
     def parse(self, response):
         json_response = json.loads(response.body_as_unicode())
         #print json_response['data']['product_name']
         code = json_response['data']['gtin_code']
-        yield {
-                'code': code,
-                'product_name': json_response['data']['product_name'],
-                'price_default': json_response['data']['price_default']
-                }
-
+        #yield {
+        #        'code': code,
+        #        'product_name': json_response['data']['product_name'],
+        #        'price_default': json_response['data']['price_default']
+        #        }
+        yield json_response['data'] 
         related_url = 'https://mining.icheck.com.vn/scan/related/{0}'.format(code)
         
         yield scrapy.Request(url = related_url, callback = self.parse_related, headers = {
